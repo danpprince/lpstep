a759d56a460372dcffe761a34fefb987883fc673
dan <danpprince@gmail.com>
2015-09-09 01:00:59 -0400

    Refactoring names
    rtmidi.midiutil    -> midiutil
    MidiInputHandler   -> MidiNoteHandler
    midi_input_handler -> midi_note_handler
    midi_out           -> lp_midi_out
    note_out           -> drum_note_out

--- py_launchpad_seq.py
+++ py_launchpad_seq.py
@@ -1,5 +1,5 @@
 import rtmidi
-import rtmidi.midiutil
+import rtmidi.midiutil as midiutil
 import time
 
 n_col = 8
@@ -8,10 +8,10 @@ n_row = 8
 bpm = 110
 
 
-class MidiInputHandler(object):
-    def __init__(self, midi_out, note_out):
-        self.midi_out = midi_out
-        self.note_out = note_out
+class MidiNoteHandler(object):
+    def __init__(self, lp_midi_out, drum_note_out):
+        self.lp_midi_out = lp_midi_out
+        self.drum_note_out = drum_note_out
         self.clear()
         # Initialize a 2d list to keep track of the state of every button
         self.button_states = [[0 for i in range(n_col)] for j in range(n_row)]
@@ -32,16 +32,16 @@ class MidiInputHandler(object):
             elif col < 8 and self.button_states[row][col] == 1:
                 # This button is currently on, turn it off
                 self.button_states[row][col] = 0
-                self.midi_out.send_message([144, note_num, 0])
+                self.lp_midi_out.send_message([144, note_num, 0])
             elif col < 8:
                 # This button is currently off, turn it on
                 self.button_states[row][col] = 1
-                self.midi_out.send_message([144, note_num, 127])
+                self.lp_midi_out.send_message([144, note_num, 127])
 
     def clear(self):
         # All LEDs are turned off, and the mapping mode, buffer settings, and 
         # duty cycle are reset to their default values. 
-        self.midi_out.send_message([176, 0, 0])
+        self.lp_midi_out.send_message([176, 0, 0])
 
     def send_tick(self, note_num, state):
         green = 3
@@ -50,37 +50,37 @@ class MidiInputHandler(object):
         row = note_num / 16
 
         if state == 1:
-            self.midi_out.send_message([144, note_num, green << 4])
+            self.lp_midi_out.send_message([144, note_num, green << 4])
 
             if self.button_states[row][col]:
-                self.note_out.send_message([144, 38, 120])
+                self.drum_note_out.send_message([144, 38, 120])
                 time.sleep(0.01)
-                self.note_out.send_message([144, 38, 0])
+                self.drum_note_out.send_message([144, 38, 0])
         else:
             if self.button_states[row][col] == 1:
                 # This button is currently on
-                self.midi_out.send_message([144, note_num, 127])
+                self.lp_midi_out.send_message([144, note_num, 127])
             else:
                 # This button is currently off
-                self.midi_out.send_message([144, note_num, 0])
+                self.lp_midi_out.send_message([144, note_num, 0])
 
 
 if __name__ == '__main__':
     # Get inputs that contain the string 'Launchpad'
-    midi_in, midi_in_name = rtmidi.midiutil.open_midiport(port='Launchpad', type_='input')
+    midi_in, midi_in_name = midiutil.open_midiport(port='Launchpad', type_='input')
     print('Opening port \'{0}\' for input'.format(midi_in_name))
 
     # Get outputs that contain the string 'Launchpad'
-    midi_out, midi_out_name = rtmidi.midiutil.open_midiport(port='Launchpad', type_='output')
-    print('Opening port \'{0}\' for output'.format(midi_out_name))
+    lp_midi_out, lp_midi_out_name = midiutil.open_midiport(port='Launchpad', type_='output')
+    print('Opening port \'{0}\' for output'.format(lp_midi_out_name))
 
     # Get outputs that contain the string 'loopMIDI'
-    note_out, note_out_name = rtmidi.midiutil.open_midiport(port='loopMIDI', type_='output')
-    print('Opening port \'{0}\' for output'.format(note_out_name))
+    drum_note_out, drum_note_out_name = midiutil.open_midiport(port='loopMIDI', type_='output')
+    print('Opening port \'{0}\' for output'.format(drum_note_out_name))
 
-    midi_input_handler = MidiInputHandler(midi_out, note_out) 
+    midi_note_handler = MidiNoteHandler(lp_midi_out, drum_note_out) 
 
-    midi_in.set_callback(midi_input_handler)
+    midi_in.set_callback(midi_note_handler)
 
     # Set the period in seconds for one sixteenth note
     period_sec = 1.0/bpm * 60 / 8
@@ -90,9 +90,9 @@ if __name__ == '__main__':
     while True:
         # Step through each button at the specified bpm
 
-        midi_input_handler.send_tick(row*16 + col, 1)
+        midi_note_handler.send_tick(row*16 + col, 1)
         time.sleep(0.1)
-        midi_input_handler.send_tick(row*16 + col, 0)
+        midi_note_handler.send_tick(row*16 + col, 0)
 
         col = col + 1
         if col >= n_col:
