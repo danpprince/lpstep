b146b642fe95f3cfaee0e5738c5ab33f32c1eca9
dan <danpprince@gmail.com>
2015-09-12 17:51:35 -0400

    Add DrumSequencer class
    DrumSequencer class is implemented in drumsequencer.py. Used to save and recall the state of steps in a single sequence.

---
+++ drumsequencer.py
@@ -0,0 +1,17 @@
+class DrumSequencer(object):
+
+    def __init__(self, sequence_length, note_num):
+        self.sequence_length = sequence_length
+        self.note_num = note_num
+
+        self.step_states = [False for i in range(sequence_length)]
+
+    def toggle(self, step):
+        self.step_states[step] = not self.step_states[step]
+        return self.step_states
+
+    def query(self, step):
+        if self.step_states[step]:
+            return self.note_num
+        else:
+            return 0
